3189eebeb833b1445723459ab78b5453e4551327
Higo Sampaio <hglbsm@gmail.com>
2021-06-30 07:10:33 -0300

    refactor: add images and update auth page style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,33 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+import { Button } from '../components/Buttons';
 import '../styles/auth.scss';
 
 export function Home() {
   return (
     <div id="page-auth">
       <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div>
-          <img src="" alt="Liveask" />
-          <button>
-            <img src="" alt="Google Icon" />
+        <div className="main-content">
+          <img src={logoImg} alt="Liveask" />
+          <Button className="create-room">
+            <img src={googleIconImg} alt="Google Icon" />
             Crie a sua sala com o Google
-          </button>
-          <div>ou entre em uma sala</div>
+          </Button>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input 
               type="text"
               placeholder="Digite o código da sala"
             />
-            <button type="submit">
+            <Button type="submit">
               Entrar na sala
-            </button>
+            </Button>
           </form>
         </div>
       </main>
