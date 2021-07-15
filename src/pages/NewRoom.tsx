c621dc8f59a829d1c1738e2ed5aa54f54545cac7
Higo Sampaio <hglbsm@gmail.com>
2021-07-15 07:17:37 -0300

    feat: add authentication with firebase and context api

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,14 +1,18 @@
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
-import { Button } from '../components/Buttons';
-import '../styles/auth.scss';
+import { Link } from "react-router-dom";
+
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Buttons";
+import "../styles/auth.scss";
 
 export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire dúvidas da sua audiência em tempo real</p>
       </aside>
@@ -17,17 +21,14 @@ export function NewRoom() {
           <img src={logoImg} alt="Liveask" />
           <h2>Criar uma nova sala</h2>
           <form>
-            <input 
-              type="text"
-              placeholder="Nome da sala"
-            />
-            <Button type="submit">
-              Criar sala
-            </Button>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
           </form>
-          <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
+          <p>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+          </p>
         </div>
       </main>
     </div>
-  )
+  );
 }
