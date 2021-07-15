a7c09544ff1494350d243ab30f048cfede1791c2
Higo Sampaio <hglbsm@gmail.com>
2021-07-14 21:49:00 -0300

    feat: add NewRoom page

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,20 +1,10 @@
-import { useHistory } from 'react-router-dom';
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
-
 import { Button } from '../components/Buttons';
-
 import '../styles/auth.scss';
 
-export function Home() {
-  const history = useHistory();
-
-  function navigateToNewRoom() {
-    history.push('/rooms/news');
-  }
-  
+export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
@@ -25,20 +15,17 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Liveask" />
-          <Button onClick={navigateToNewRoom} className="create-room">
-            <img src={googleIconImg} alt="Google Icon" />
-            Crie a sua sala com o Google
-          </Button>
-          <div className="separator">ou entre em uma sala</div>
+          <h2>Criar uma nova sala</h2>
           <form>
             <input 
               type="text"
-              placeholder="Digite o código da sala"
+              placeholder="Nome da sala"
             />
             <Button type="submit">
-              Entrar na sala
+              Criar sala
             </Button>
           </form>
+          <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
         </div>
       </main>
     </div>
