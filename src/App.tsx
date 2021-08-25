19fb86bf53479a0206aa0fd7428ccc3a2054bd48
Higo Sampaio <hglbsm@gmail.com>
2021-08-24 22:14:01 -0300

    feat: add GlobalStyle with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import { AuthContextProvider } from "./contexts/AuthContext";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { AuthContextProvider } from "./contexts/AuthContext";
 import { Room } from "./pages/Room";
 import { AdminRoom } from "./pages/AdminRoom";
 
